8f75abb7741f87c3e02de68beeb1f6036d0b1ed6
ShamievDavid <shamievdave@gmail.com>
2025-10-31 22:41:22 +0200

    minors

--- app/views/ArchiveView.tsx
+++ app/views/ArchiveView.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client'
 
 import { ArchiveProject as ArchiveProjectType } from "@/sanity/lib/client"
--- app/views/GalleryView.tsx
+++ app/views/GalleryView.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 'use client'
 
 import { Project as ProjectType } from '@/types';
