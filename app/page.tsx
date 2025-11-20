ae4fe7f47115acd6843ec42066302b446a46bb07
ShamievDavid <shamievdave@gmail.com>
2025-11-20 15:32:11 +0200

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 import { getHighlightsCached } from '@/sanity/lib/client';
-import { Home as HomeView } from './views';
+// import { Home as HomeView } from './views';
 
 export default async function Page() {
   const highlights = await getHighlightsCached();
 
-  return <HomeView projects={highlights} />;
+  return (
+    <div className="h-[100dvh] snap-y snap-mandatory overflow-y-auto">
+      {highlights.map((project) => (
+        <div
+          className="w-full h-full flex items-center justify-center bg-yellow-200 snap-start p-4"
+          key={project._id}
+        >
+          {project.title}
+        </div>
+      ))}
+    </div>
+  );
 }
