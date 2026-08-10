3253e601e4febc68836d38f6a9fb619f2b36a44d
lymanballif <lyman@newapology.com>
2026-08-10 16:35:26 -0600

    Rename Cole Ferguson Sanity Studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -14,6 +14,7 @@ import {schema} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
 
 export default defineConfig({
+  title: 'Cole Ferguson',
   basePath: '/studio',
   projectId,
   dataset,
